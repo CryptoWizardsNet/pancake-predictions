af88b17ef55b9a7378c42eb2299ac91ca297c3d5
Shaun McDonogh <shaun@Shauns-MacBook-Pro.local>
2023-02-04 07:46:13 +0000

    upgrades

--- strategy/constants.py
+++ strategy/constants.py
@@ -3,13 +3,13 @@ from decouple import config
 import json
 
 # TRAIN
-SAVE_HISTORY = False
+SAVE_HISTORY = True
 RUN_MODEL_1 = True
-RUN_MODEL_2 = False
+RUN_MODEL_2 = True
 
 # EXECUTION
 SCAN_OPPORTUNITIES = True
-IS_EXECUTE_TRADE = False
+IS_EXECUTE_TRADE = True
 
 # Get ABI
 with open("abi.json", "r") as myFile:
